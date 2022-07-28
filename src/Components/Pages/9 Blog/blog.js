39ec6e7689c691e0abb06f1d032a03afb6682c68
Tusharadapa <84062643+Tusharadapa@users.noreply.github.com>
2022-07-28 10:55:02 +0530

    Blog API Implemented

--- src/Components/Pages/9 Blog/blog.js
+++ src/Components/Pages/9 Blog/blog.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Header from "../../HomePage/Header/Header";
-// import Bodyblog from "./Bodyblog";
 import Footer from "../../HomePage/Footer/footer";
 import BodyBlog1 from "./BodyBlog1";
 
@@ -8,7 +7,6 @@ const Blog = () => {
   return (
     <>
       <Header />
-      {/* <Bodyblog /> */}
       <BodyBlog1 />
       <Footer />
     </>
